e60a8891491959cbfb99c3214b594c7769b7a4d1
MohammadFayasKhan <fayaskhanmohammad@gmail.com>
2026-07-06 20:10:30 +0530

    Switch recording mechanism to Toggle Mode (tap-to-start, tap-to-stop) to resolve touch sensor release dropouts

--- FayasAI.ino
+++ FayasAI.ino
@@ -197,14 +197,14 @@ static void handleListening() {
 
   FayasAnimations::renderListening(elapsed);
 
-  if (g_button.wasReleased() || bufferFull) {
+  if (g_button.wasPressed() || bufferFull) {
     FayasAudio::stopRecording();
     size_t recordedBytes = FayasAudio::getRecordedByteCount();
 
     if (bufferFull) {
       Serial.println(F("[Listen] Buffer full (max recording reached)"));
     } else {
-      Serial.println(F("[Listen] >>> BUTTON RELEASED <<<"));
+      Serial.println(F("[Listen] >>> BUTTON PRESSED AGAIN (STOP) <<<"));
     }
     Serial.printf("[Listen] Recorded %u bytes (%.1f KB)\n", recordedBytes,
                   recordedBytes / 1024.0f);
